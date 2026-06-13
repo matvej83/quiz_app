ffc6656da7c4ae09f0683e6186690715bf58e262
Vladimir Matveenko <vladimir.m@thecomity.com>
2026-06-13 13:49:23 +0300

    small UI improvements

--- lib/features/quiz/presentation/widgets/answer_result.dart
+++ lib/features/quiz/presentation/widgets/answer_result.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_app/app/theme/app_semantic_colors.dart';
 
@@ -5,20 +6,22 @@ class AnswerResult extends StatelessWidget {
   const AnswerResult({
     super.key,
     required this.isCorrect,
-    this.correctText = 'Правильно',
-    this.incorrectText = 'Неправильно',
+    this.correctText,
+    this.incorrectText,
   });
 
   final bool isCorrect;
-  final String correctText;
-  final String incorrectText;
+  final String? correctText;
+  final String? incorrectText;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final semanticColors = theme.extension<AppSemanticColors>();
     return Text(
-      isCorrect ? correctText : incorrectText,
+      isCorrect
+          ? (correctText ?? 'testPage.correct'.tr())
+          : (incorrectText ?? 'testPage.incorrect'.tr()),
       style: TextStyle(
         color: isCorrect ? semanticColors!.success : theme.colorScheme.error,
       ),
