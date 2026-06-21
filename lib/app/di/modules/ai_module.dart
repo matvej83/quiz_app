c08bab74febad676ec69d92ad20b2baf1e907114
Vladimir Matveenko <vladimir.m@thecomity.com>
2026-06-21 14:08:58 +0300

    consts changed

--- lib/app/di/modules/ai_module.dart
+++ lib/app/di/modules/ai_module.dart
@@ -1,12 +1,14 @@
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:injectable/injectable.dart';
 
+import '../../constants/app_constants.dart';
+
 @module
 abstract class AiModule {
   @lazySingleton
   GenerativeModel generativeModel() {
     return GenerativeModel(
-      model: 'gemini-2.5-flash',
+      model: AppConstants.aiModelName,
       apiKey: const String.fromEnvironment('GEMINI_API_KEY'),
       generationConfig: GenerationConfig(responseMimeType: 'application/json'),
     );
