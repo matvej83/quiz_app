43aa69e910157322df7445ef63be4fab4a45aef5
Vladimir Matveenko <vladimir.m@thecomity.com>
2026-06-26 17:00:59 +0300

    gemini errors parser changed

--- test/test.dart
+++ test/test.dart
@@ -1,5 +1,11 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
 import 'package:quiz_app/app/utils/app_utils.dart';
+import 'package:quiz_app/core/error/exception.dart';
 
 void main() {
   group('AppUtils.parseJson', () {
@@ -41,6 +47,59 @@ void main() {
       };
       final parsed = AppUtils.parseJson(json);
       expect(result, parsed);
+    });
+  });
+
+  group('AppUtils.parseClientError', () {
+    test('SocketException', () {
+      final result = AppUtils.parseClientError(const SocketException('text'));
+      final expected = 'errors.network.noInternet'.tr();
+
+      expect(result, expected);
+    });
+    test('TimeoutException', () {
+      final result = AppUtils.parseClientError(TimeoutException('text'));
+      final expected = 'errors.network.timeout'.tr();
+
+      expect(result, expected);
+    });
+    test('HandshakeException', () {
+      final result = AppUtils.parseClientError(
+        const HandshakeException('text'),
+      );
+      final expected = 'errors.network.sslError'.tr();
+
+      expect(result, expected);
+    });
+    test('http.ClientException', () {
+      final result = AppUtils.parseClientError(http.ClientException('text'));
+      final expected = 'errors.network.connectionError'.tr();
+
+      expect(result, expected);
+    });
+    test('FormatException', () {
+      final result = AppUtils.parseClientError(const FormatException('text'));
+      final expected = 'errors.geminiErrors.formatException'.tr();
+
+      expect(result, expected);
+    });
+    test('TypeError', () {
+      final result = AppUtils.parseClientError(TypeError());
+      final expected = 'errors.geminiErrors.invalidResponse'.tr();
+
+      expect(result, expected);
+    });
+    test('RangeError', () {
+      final result = AppUtils.parseClientError(RangeError('text'));
+      final expected = 'errors.geminiErrors.invalidResponse'.tr();
+
+      expect(result, expected);
+    });
+    test('UnknownException', () {
+      final result = AppUtils.parseClientError(UnknownException('text'));
+      final expected = 'errors.geminiErrors.unknownError'.tr();
+
+      expect(result, expected);
     });
   });
 }
