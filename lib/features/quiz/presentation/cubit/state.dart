6acc26c170833ce5467057c9a9f3cfbe34eaad50
Vladimir Matveenko <matvej83@gmail.com>
2026-08-12 22:07:27 +0300

    state changed with freezed

--- lib/features/quiz/presentation/cubit/state.dart
+++ lib/features/quiz/presentation/cubit/state.dart
@@ -1,70 +1,22 @@
-import 'package:equatable/equatable.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:quiz_app/features/quiz/presentation/cubit/cubit.dart';
 
 import '../../../dictionary/data/database/app_database.dart';
 
-class QuizState extends Equatable {
-  const QuizState({
-    this.status = QuizStatus.initial,
-    this.words = const [],
-    this.additionalWords = const [],
-    this.answers = const [],
-    this.currentIndex = 0,
-    this.answered = false,
-    this.correct = false,
-    this.userAnswer,
-    this.correctCount = 0,
-    this.errorMessage,
-  });
+part 'state.freezed.dart';
 
-  final QuizStatus status;
-  final List<Word> words;
-  final List<String> additionalWords;
-  final List<String> answers;
-  final int currentIndex;
-  final bool answered;
-  final bool correct;
-  final String? userAnswer;
-  final int correctCount;
-  final String? errorMessage;
-
-  QuizState copyWith({
-    QuizStatus? status,
-    List<Word>? words,
-    List<String>? additionalWords,
-    List<String>? answers,
-    int? currentIndex,
-    bool? answered,
-    bool? correct,
-    String? userAnswer,
-    int? correctCount,
+@freezed
+abstract class QuizState with _$QuizState {
+  const factory QuizState({
+    @Default(QuizStatus.initial) QuizStatus status,
+    @Default([]) List<Word> words,
+    @Default([]) List<String> additionalWords,
+    @Default([]) List<String> answers,
+    @Default(0) int currentIndex,
+    @Default(0) int correctCount,
     String? errorMessage,
-  }) {
-    return QuizState(
-      status: status ?? this.status,
-      words: words ?? this.words,
-      additionalWords: additionalWords ?? this.additionalWords,
-      answers: answers ?? this.answers,
-      currentIndex: currentIndex ?? this.currentIndex,
-      answered: answered ?? this.answered,
-      correct: correct ?? this.correct,
-      userAnswer: userAnswer ?? this.userAnswer,
-      correctCount: correctCount ?? this.correctCount,
-      errorMessage: errorMessage ?? this.errorMessage,
-    );
-  }
-
-  @override
-  List<dynamic> get props => [
-    status,
-    words,
-    additionalWords,
-    answers,
-    currentIndex,
-    answered,
-    correct,
-    userAnswer,
-    correctCount,
-    errorMessage,
-  ];
+    String? userAnswer,
+    @Default(false) bool answered,
+    @Default(false) bool correct,
+  }) = _QuizState;
 }
